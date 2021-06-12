aa14d38c016c2ed08cd9b370b259673fcb9b91bc
MingHHsu <ming.hs.hsu@gmail.com>
2021-06-13 01:30:25 +0800

    update Landing Page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 import {
   LandingRoot,
-  LinkToLobby,
   LinkButton,
   UsernameInput,
 } from './styled';
 
 export default function Landing() {
+  const dispatch = useDispatch();
   const [username, setUsername] = useState('');
+  const handleLink = () => dispatch(push('/lobby'));
   return (
     <LandingRoot>
       <UsernameInput value={username} onChange={(e) => setUsername(e.target.value)} />
-      <LinkButton onClick={() => {}}>
-        <LinkToLobby to="/lobby">進入大廳</LinkToLobby>
-      </LinkButton>
+      <LinkButton onClick={handleLink}>進入大廳</LinkButton>
     </LandingRoot>
   );
 }
--- src/pages/Landing/styled.js
+++ src/pages/Landing/styled.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import {
   Button,
   Input,
 } from 'components/common';
 
 export const LandingRoot = styled.div`
-`;
-
-export const LinkToLobby = styled(Link)`
 `;
 
 export const LinkButton = styled(Button)`
